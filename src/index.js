15778d366f4a859ea0057d08d80c7cf9b2f72830
mikedim95 <mikedim95@gmail.com>
2023-08-22 16:19:57 +0300

    ...correcting

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import {disableReactDevTools} from '@fvilers/disable-react-devtools'
-if (ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
