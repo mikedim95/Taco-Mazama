9e8deafc92fa4aa2a43dbbb25d338b93cd8b200e
mikedim95 <mikedim95@gmail.com>
2023-08-22 16:11:56 +0300

    disable-react-devtools for deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+if (ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
