6c632ffacf256c5a57f5415bb55601738481bb92
dakmalandro <dakmalandro86@gmail.com>
2023-09-18 23:00:49 +0300

    test2 with back

--- src/helpers/functionalComponents/postRequestToBack.js
+++ src/helpers/functionalComponents/postRequestToBack.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-const API_URL = "https://taco-mazama-api.onrender.com:3000/order"; // Replace with your API endpoint
+const API_URL = "https://taco-mazama-api.onrender.com/order"; // Replace with your API endpoint
 
 const postJsonData = async (jsonData) => {
   try {
