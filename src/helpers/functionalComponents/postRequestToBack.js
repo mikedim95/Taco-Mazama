b67d0fc0a1063861368296a2cc5d6e30457ed52c
mikedim95 <mikedim95@gmail.com>
2023-09-23 18:14:43 +0300

    testing

--- src/helpers/functionalComponents/postRequestToBack.js
+++ src/helpers/functionalComponents/postRequestToBack.js
@@ -1,15 +1,16 @@
-// api.js
 import axios from "axios";
-const API_URL = process.env.API_URL;
 
 const postJsonData = async (jsonData) => {
-  console.log(jsonData);
   try {
-    const response = await axios.post(API_URL, jsonData, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await axios.post(
+      "https://taco-mazama-api.onrender.com/order",
+      jsonData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     console.log(response.data);
   } catch (error) {
     console.error("Error:", error);
