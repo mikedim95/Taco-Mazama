504b3a3fc691b6e8476b20f0042980083b266ea0
mikedim95 <mikedim95@gmail.com>
2023-09-23 15:38:48 +0300

    testibg

--- src/helpers/functionalComponents/postRequestToBack.js
+++ src/helpers/functionalComponents/postRequestToBack.js
@@ -1,8 +1,8 @@
 // api.js
 import axios from "axios";
 
-/* const API_URL = "https://taco-mazama-api.onrender.com/order"; // Replace with your API endpoint */
-const API_URL = "http://localhost:4000/order"; // Replace with your API endpoint
+const API_URL = "https://taco-mazama-api.onrender.com/order"; // Replace with your API endpoint */
+/* const API_URL = "http://localhost:4000/order"; */ // Replace with your API endpoint
 const postJsonData = async (jsonData) => {
   console.log(jsonData);
   try {
